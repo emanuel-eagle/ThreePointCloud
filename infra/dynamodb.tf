27dbce717a4a3a678f45170f8a339aa4dd642002
Emanuel Eagle <emanuel.eagle@gmail.com>
2025-03-12 22:45:27 -0400

    update

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -1,6 +1,8 @@
 resource "aws_dynamodb_table" "dynamodb-table" {
   name           = "player-database"
   billing_mode   = "PROVISIONED"
+  read_capacity = 1000
+  write_capacity = 1000
   hash_key       = "player-id"
 
   attribute {
