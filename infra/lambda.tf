d781a26a4504c683d3997c81954281a61222277e
Emanuel Eagle <emanuel.eagle@gmail.com>
2025-03-13 23:47:48 -0400

    update

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -114,7 +114,7 @@ resource "aws_lambda_function" "threepointcloud_careerstats_coordinator_collecti
 resource "aws_lambda_function" "threepointcloud_gamestats_collection" {
   function_name = var.lambda_function_name_game_stats_collection
   role          = aws_iam_role.iam_for_lambda.arn
-  image_uri     = "${aws_ecr_repository.three-point-cloud-game-stats-container-repository.repository_url}:${var.lambda_image_tag_gamelogStats}"
+  image_uri     = "${aws_ecr_repository.three-point-cloud-game-stats-collection-container-repository.repository_url}:${var.lambda_image_tag_gamelogStats}"
   package_type = var.package_type
   timeout = var.career_stats_lambda_timeout
   memory_size = var.career_stats_lambda_memory_size
